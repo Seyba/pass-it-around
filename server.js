dad101fe70e1f43e20d4da022504240fbb78d329
Mahamadou <mmohmo@yahoo.com>
2023-08-24 23:02:21 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+
+//* Routes
+app.get('/', (req, res) => {
+    res.send(`<h1>99 Bottles of beer on the wall. <a href="/98">take one down</a></h1>`)
+})
+
+app.get('/:numb', (req, res) => {
+    const n = req.params.numb
+    const updatedNumb = n-1
+    console.log(typeof updatedNumb)
+    res.send(`Number of bottles is ${updatedNumb}`)
+})
+
+app.listen(port, () => {
+    console.log(`App is running on port: ${port}`)
+})
